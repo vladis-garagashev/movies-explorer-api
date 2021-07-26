1a0bfc5dd9e7afed75c0d4322739cf8050ac90e5
Vladislav Garagashev <vladis.garagashev@gmail.com>
2021-07-26 16:39:56 +0500

    feat: Ддобавить схему модели фильма

---
+++ models/movie.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const { linkRegExp } = require('../utils/constants');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => linkRegExp.test(link),
+      message: 'Вставьте ссылку на постер фильма',
+    },
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => linkRegExp.test(link),
+      message: 'Вставьте ссылку на трейлер фильма',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => linkRegExp.test(link),
+      message: 'Вставьте ссылку на миниатюру постера фильма',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+
+});
+
+module.exports = mongoose.model('movie', movieSchema);
--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const { linkRegExp } = require('../utils/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
