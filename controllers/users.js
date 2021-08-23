d10330c7e56f34c1b29e50707b652d10e4ea40d2
Vladislav Garagashev <vladis.garagashev@gmail.com>
2021-08-23 18:51:29 +0500

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -31,6 +31,7 @@ const login = async (req, res, next) => {
       .cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
+        sameSite: 'Lax',
       })
       .send({
         _id: user._id,
